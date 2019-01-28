0afc13753b4e072dc5495756dc0bc64c1a687262
Marshalla R <34112391+MellowMarsh@users.noreply.github.com>
2019-01-28 16:38:38 -0600

    fixed typo

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -6,7 +6,7 @@ class Book extends React.Component {
             <li>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 130, height: 192, backgroundImage: `url("${(this.props.book.imageLinks && this.props.book.imageLinks.thumbnail) || "" }")` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${(this.props.book.imageLinks && this.props.book.imageLinks.thumbnail) || "" }")` }}></div>
                     <div className="book-shelf-changer">
                         {/*Call for updateBook method*/}
                         <select value ={this.props.book.shelf || "None"} onChange={(event) => {this.props.updateBook(this.props.book, event.target.value) }}> {}
